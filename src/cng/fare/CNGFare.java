02dce411d082bceeca2a98d8ff6792b538181c00
brightuzzal <brightuzzal09@gmail.com>
2017-02-08 10:09:34 +0600

    Add files via upload

---
+++ src/cng/fare/CNGFare.java
@@ -0,0 +1,20 @@
+
+package cng.fare;
+
+/**
+ *
+ * @author MASUD
+ */
+public class CNGFare
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        new CNG().setVisible(true);
+    }
+    
+}
